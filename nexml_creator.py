9257f55ede47497f8185886c76dc0b6ca6e788d2
dudugan <dvzar27@gmail.com>
2024-11-07 10:53:35 -0500

    fixed stuff

--- nexml_creator.py
+++ nexml_creator.py
@@ -3,39 +3,39 @@ import dendropy as dd
 import sys
 
 def create_nexml(pokemon_list):
-    char_matrix = dd.CharacterMatrix()
+    char_matrix = dd.StandardCharacterMatrix()
 
     for name in pokemon_list:
         pokemon = pb.pokemon(name)
 
         taxon = dd.Taxon(label=name.capitalize())
-        char_row = char_matrix.new_characters_for_taxon(taxon)
+        char_matrix[taxon] = []
 
         # height, weight as continuous data
-        char_row["height"] = float(pokemon.height)
-        char_row["weight"] = float(pokemon.weight)
+        char_matrix[taxon].append(float(pokemon.height))
+        char_matrix[taxon].append(float(pokemon.weight))
 
-        # abilities, types, moves as categorical sets
-        char_row["abilities"] = {a.ability.name for a in pokemon.abilities}
-        char_row["types"] = {t.type.name for t in pokemon.types}
-        # only first 10 moves
-        char_row["moves"] = {m.move.name for m in pokemon.moves[:10]}
+        abilities = {a.ability.name for a in pokemon.abilities}
+        types = {t.type.name for t in pokemon.types}
+        moves = {m.move.name for m in pokemon.moves[:10]}
+        games = {g.version.name for g in pokemon.game_indices[:10]}
+
+
+        # right now this weights height the same as each individual ability; maybe change
+        char_matrix[taxon].extend(abilities)
+        char_matrix[taxon].extend(types)
+        char_matrix[taxon].extend(moves)
+        char_matrix[taxon].extend(games)
 
         # location area encounters as a list, also limited to 10
         locations = pb.APIResource('pokemon', name).location_area_encounters()
         location_areas = {loc["location_area"]["name"] for loc in locations[:10]} if locations else {"unknown"}
-        char_row["locations"] = location_areas
-
-        # game indices as categorical
-        char_row["games"] = {g.version.name for g in pokemon.game_indices}
+        char_matrix[taxon].extend(location_areas)
 
         # add stats as individual continuous data
         # TODO: check if all pokemon have same number of stats
         for stat in pokemon.stats:
-            char_row[f"stat_{stat.stat.name}"] = float(stat.base_stat)
-
-        # add taxon to NeXML dataset
-        char_matrix.append(char_row)
+            char_matrix[taxon].append(float(stat.base_stat))
     
     # write to NeXML file
     nexml_tree = dd.DataSet()
@@ -43,12 +43,9 @@ def create_nexml(pokemon_list):
     nexml_tree.write(path="pokedata.xml", schema="nexml")
     print("NeXML file created as 'pokedata.xml")
 
-pokemon_list = []
+pokemon_list = sys.argv[1:] if len(sys.argv) > 1 else []
 
-if len(sys.argv) > 1:
-    for arg in sys.argv[1:]:
-        pokemon_list.append(arg)
-else:
+if not pokemon_list: 
     print("no command-line pokemon!")
 
 create_nexml(pokemon_list)
