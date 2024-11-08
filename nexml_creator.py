590231c4ae81a2b41bfe50b2e98a86be9cad4788
dudugan <dvzar27@gmail.com>
2024-11-08 11:14:42 -0500

    better?

--- nexml_creator.py
+++ nexml_creator.py
@@ -37,6 +37,45 @@ def create_nexml(pokemon_list):
         
         # add this stat's matrix to the dataset
         nexml_tree.add_char_matrix(stats_matrices[f"{stat_name}_matrix"])
+
+    # add all possible types, moves, abilities, game_indices and locations
+    types_set = set()
+    abilities_set = set()
+    moves_set = set()
+    locations_set = set()
+    games_set = set()
+
+    # populate categorical sets
+    for pokemon in pokemons:
+        for ability in pokemon.abilities:
+            abilities_set.add(ability.name)
+        for move in pokemon.moves:
+            moves_set.add(move.name)
+        for type in pokemon.types:
+            types_set.add(type.name)
+        for game in pokemon.game_indices:
+            games_set.add(game.version.name)
+        encounters_url = pokemon.location_area_encounters
+        locations = []
+        for i in range(len(encounters_url)):
+            location = encounters_url[i].location_area.name
+            locations.append(location)
+        for loc in locations:
+            locations_set.add(loc)
+
+    # make sets into lists to maintain order
+    abilities_list = list(abilities_set)
+    moves_list = list(moves_set)
+    types_list = list(types_set)
+    games_list = list(games_set)
+    locs_list = list(locations_set)
+
+    # create dictionary for each thing with name as key and index in list as value
+    abilities_map = {ability: idx for idx, ability in enumerate(abilities_list)}
+    moves_map = {move: idx for idx, move in enumerate(moves_list)}
+    types_map = {type_: idx for idx, type_ in enumerate(types_list)}
+    games_map = {move: idx for idx, move in enumerate(games_list)}
+    locs_map = {type_: idx for idx, type_ in enumerate(locs_list)}
         
     # add each pokemon's data
     for pokemon in pokemons:
@@ -51,37 +90,31 @@ def create_nexml(pokemon_list):
             stats_matrices[f"{stat_name}_matrix"][taxon] = [float(stat.base_stat)]
 
         # read in categorical data and add to matrices
-        abilities = {a.ability.name for a in pokemon.abilities}
-        types = {t.type.name for t in pokemon.types}
-        moves = {m.move.name for m in pokemon.moves[:10]}
-        games = {g.version.name for g in pokemon.game_indices[:10]}
+        # abilities = {a.ability.name for a in pokemon.abilities[:10]}
+        # types = {t.type.name for t in pokemon.types}
+        # moves = {m.move.name for m in pokemon.moves[:10]}
+        # games = {g.version.name for g in pokemon.game_indices[:10]}
 
-        abilities_matrix[taxon] = list(abilities)
-        types_matrix[taxon] = list(types)
-        moves_matrix[taxon] = list(moves)
-        games_matrix[taxon] = list(games)
+        # Read in categorical data (abilities, types, moves, games, and locations) and add to matrices
+            # -1 is returned if get doesn't find anything
+        abilities_indices = [abilities_map.get(ability.name, -1) for ability in pokemon.abilities[:10]]
+        types_indices = [types_map.get(type_.name, -1) for type_ in pokemon.types]
+        moves_indices = [moves_map.get(move.name, -1) for move in pokemon.moves[:10]]  
+        games_indices = [games_map.get(game.version.name, -1) for game in pokemon.game_indices[:10]] 
 
-        # location area encounters as a list, also limited to 10
         encounters_url = pokemon.location_area_encounters
         locations = []
-        
         for i in range(len(encounters_url)):
             location = encounters_url[i].location_area.name
-            print(location)
             locations.append(location)
-            print(locations)
+        locations_indices = [locs_map.get(loc, -1) for loc in locations[:10]]
 
-        # if encounters_url:
-        #     response = rq.get(encounters_url)
-        #     if response.status_code == 200:
-        #         locations = response.json()
-        #         location_areas = {loc["location_area"]["name"] for loc in locations[:10]} if locations else {"unknown"}
-        #     else:
-        #         location_areas = {"unknown"}
-        # else:
-        #     location_areas = {"unknown"}
-
-        locations_matrix[taxon] = list(locations)
+        # Assign the indices to the appropriate matrices
+        abilities_matrix[taxon] = abilities_indices
+        types_matrix[taxon] = types_indices
+        moves_matrix[taxon] = moves_indices
+        games_matrix[taxon] = games_indices
+        locations_matrix[taxon] = locations_indices
     
     # write NeXML file
     nexml_tree.write(path="pokedata.xml", schema="nexml")
