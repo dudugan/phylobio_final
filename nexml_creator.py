f5d4c037e202e152acad74966273462794da4b5a
dudugan <dvzar27@gmail.com>
2024-11-08 10:03:30 -0500

    combined methods

--- nexml_creator.py
+++ nexml_creator.py
@@ -62,11 +62,11 @@ def create_nexml(pokemon_list):
         games_matrix[taxon] = list(games)
 
         # location area encounters as a list, also limited to 10
-        # encounters_url = pokemon.location_area_encounters()
+        encounters_url = pokemon.location_area_encounters
         # splitting by slash gives us the last two things in the url
-        # locations = pb.APIResource(encounters_url.split('/')[-2], encounters_url('/')[-1])
+        locations = pb.APIResource(encounters_url.split('/')[-2], encounters_url('/')[-1])
 
-        locations = pokemon.location_area_encounters
+        # locations = pokemon.location_area_encounters
         location_areas = {loc["location_area"]["name"] for loc in locations[:10]} if locations else {"unknown"}
         
         
