bf7bde351494fa8ed374377c9fc77fa80c0d0350
dudugan <dvzar27@gmail.com>
2024-11-12 20:28:29 -0500

    if has stats

--- nexus_creator.py
+++ nexus_creator.py
@@ -91,10 +91,11 @@ def get_data(pokemon_list):
             print(f"{pokemon.name} does not have weight attribute")
 
         for stat in pokemon.stats:
-            if (stats_dict[stat.stat.name] == []):
-                stats_dict[stat.stat.name] = [float(stat.base_stat)]
-            else:
-                stats_dict[stat.stat.name].append(float(stat.base_stat))
+            if (hasattr(pokemon, 'stats')):
+                if (stats_dict[stat.stat.name] == []):
+                    stats_dict[stat.stat.name] = [float(stat.base_stat)]
+                else:
+                    stats_dict[stat.stat.name].append(float(stat.base_stat))
 
     # TODO: actually in the end test stats by distribution, 
     # ie get the sum of all stats and then get the % of that that is HP, attack, etc.
